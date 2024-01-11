3ab6538ce730a31f01c2bfe472e8c2b4642d8f61
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-11 13:22:46 -1000

    add carousel class component

---
+++ src/Carousel.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0
+  }
+
+  static defaultProps = {
+    images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
+  }
+
+  render () {
+    const { active } = this.state;
+    // ^vs. useState => which is a hook (no-no in class components)
+    const { images } = this.props;
+    // ^ passed in from parent
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? 'active' : ''}
+              alt="animal-thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Carousel
