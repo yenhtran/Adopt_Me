2996d5353958a5947d8f9d6d7b231b0c43cc18c7
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-10 14:15:44 -1000

    react router, useQuery

---
+++ src/Details.jsx
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
+const Details = () => {
+  const { id } = useParams();
+  const results = useQuery(['details', id], fetchPet);
+  // ^ this is cool because we get to treat this like a synchronous hook, but it's fetching from an asynchronous API
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    )
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state} </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  )
+};
+
+export default Details;
